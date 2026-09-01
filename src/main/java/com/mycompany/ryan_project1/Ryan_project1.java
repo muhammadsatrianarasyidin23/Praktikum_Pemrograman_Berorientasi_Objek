9db6ae7f8d78f4cad885895759bedbf03cafb999
muhammadsatrianarasyidin23 <rjason@gmail.com>
2026-09-02 03:08:49 +0700

    ryan_project1

---
+++ src/main/java/com/mycompany/ryan_project1/Ryan_project1.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ryan_project1;
+
+/**
+ *
+ * @author T480 I7 GEN 8
+ */
+public class Ryan_project1 {
+
+    public static void main(String[] args) {
+        System.out.println("Nama   : Muhammad Satryana Rasyidin");
+        System.out.println("NPM    : 2505060018");
+        System.out.println("Alamat : Mertoyudan");  
+        System.out.println("No_HP  : 085228371218");
+    }
+}
